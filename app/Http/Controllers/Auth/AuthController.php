bd98b63fae8094b73e14524abb4f1167446f07d8
gimgyeongtae <qlalf258@naver.com>
2022-02-09 15:00:38 +0900

    .env

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use App\Models\User;
 use Illuminate\Support\Facades\Log;
 use Validator;
 
